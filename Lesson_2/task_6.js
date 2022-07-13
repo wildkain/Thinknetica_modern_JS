c5189b2bbadb9913e364ec4b5e7541c79303abb6
wildkain <wildkain@gmail.com>
2022-07-13 11:04:09 +0300

    review issue task_6

--- Lesson_2/task_6.js
+++ Lesson_2/task_6.js
@@ -1,7 +1,7 @@
 // Реализовать функцию для перемешивания элементов массива
 
 function arrayShuffle(array) {
-    return array.sort(() => Math.round(Math.random()) * 2 - 1)
+    return array.sort(() => Math.round(Math.random()) - 0.5)
 }
 
 console.log(arrayShuffle([1, 2, 3, 4, 6, 7, 8, 9]))
