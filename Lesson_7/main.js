c8c2766ea232a1e55b46d0fdd0c9a252c70a01f0
wildkain <wildkain@gmail.com>
2022-08-14 16:13:17 +0300

    add new programming interface

--- Lesson_7/main.js
+++ Lesson_7/main.js
@@ -1,7 +1,11 @@
 document.ondragstart = noselect;
 document.onselectstart = noselect;
 document.oncontextmenu = noselect;
-function noselect() {return false;}
+
+function noselect() {
+   alert('Prohibited !!!')
+    return false;
+}
 
 const display = document.querySelector('.display')
 const buttonsBlock = document.querySelector('.button-block')
@@ -12,30 +16,33 @@ let previousOperand = null;
 let currentOperation = null;
 let result;
 
-const performOperation = (operation) => {
-    switch (operation) {
-        case '+':
-            result = Number(previousOperand) + Number(currentOperand)
-            break;
-        case "-":
-            result = Number(previousOperand) - Number(currentOperand)
-            break;
-        case "/":
-            if (currentOperand == 0) {
-                alert('ZeroDivision is prohibited')
-                result = 0
-                break;
-            }
-            result = Number(previousOperand) / Number(currentOperand)
-            break;
-        case "*":
-            result = Number(previousOperand) * Number(currentOperand)
-            break;
-        default:
-            alert('Unsupported operation')
-            break;
+const AvailableOperations = {
+    '+': () => {
+        return previousOperand + currentOperand
+    },
+    '-': () => {
+        return  previousOperand - currentOperand
+    },
+    '/': () => {
+        if (currentOperand == 0) {
+            alert('ZeroDivision is prohibited')
+            result = 0
+        }
+        return Number(previousOperand) / Number(currentOperand)
+    },
+    '*': () => {
+        return Number(previousOperand) * Number(currentOperand)
     }
-    return result;
+}
+
+const performOperation = (operation) => {
+    default_result = () => {
+        alert('Unsupported operation')
+    }
+    result = AvailableOperations[operation] || default_result
+    console.log('RESULT')
+    console.log(result)
+    return result();
 }
 
 const performInputOperation = keyValue => {
@@ -56,18 +63,23 @@ const performInputOperation = keyValue => {
             clearDisplay();
             break;
         case "=":
-            performOperation(currentOperation);
+            result = performOperation(currentOperation);
+            display.value = result
             currentOperand = null;
             previousOperand = null;
-            display.value = result;
             break;
         default:
             if (Number.isInteger(Number(keyValue))) {
-                currentOperand = (currentOperand === null) ? keyValue : currentOperand + keyValue
-                console.debug(currentOperand)
+                currentOperand = (currentOperand === null) ? Number(keyValue) : Number(currentOperand + keyValue)
                 display.value = currentOperand
-            } else {
+            } else if (AvailableOperations[keyValue] == undefined) {
                 alert('incorrect input')
+            } else {
+                previousOperand = Number(currentOperand)
+                currentOperand = null
+                currentOperation = keyValue
+                display.value = Number(currentOperand)
+                break;
             }
     }
 }
@@ -87,18 +99,67 @@ buttonsBlock.addEventListener('click', (event) => {
     }
 )
 
+const add_operation_button = document.querySelector('.add_op_button')
+
+add_operation_button.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('U here');
+    const add_operation_form = document.querySelector('.add_operation_box')
+    const current_style = add_operation_form.style.display
+    switch (current_style) {
+        case 'none':
+            add_operation_form.style.display = 'block'
+            break;
+        default:
+            add_operation_form.style.display = 'none'
+            break;
+    }
+})
+
 window.onkeydown = (event) => {
-    if (event.key == 'Backspace') {
+    if (event.key == 'Backspace' && !isNewOperationAdding()) {
         event.preventDefault();
         clearDisplay(1);
-    } else if (event.key == "Enter") {
+    } else if (event.key == "Enter" && !isNewOperationAdding()) {
         event.preventDefault();
         performInputOperation("=")
     }
 }
 
+const isNewOperationAdding = () => {
+    return document.querySelector('.add_operation_box').style.display == 'block'
+}
+
+const hideNewOperationForm = () => {
+    document.querySelector('.add_operation_box').style.display = 'none'
+}
+
+document.addEventListener('submit', event => {
+    event.preventDefault()
+    const form = new FormData(event.target);
+    const identifier = form.get("identifier");
+    const function_body = form.get("function_body");
+    addNewButtonToButtonsBlock(identifier)
+    addNewOperationFunction(identifier, function_body)
+
+})
+
+const addNewOperationFunction = (identifier, function_body) => {
+    AvailableOperations[identifier] = eval(function_body)
+    console.debug(AvailableOperations)
+}
+
+const addNewButtonToButtonsBlock = (identifier) => {
+    let example = buttonsBlock.children[0]
+    let new_button = example.cloneNode(true)
+    new_button.innerHTML = identifier
+    buttonsBlock.appendChild(new_button)
+    isNewOperationAdding() ? hideNewOperationForm() : null
+}
+
 document.addEventListener('keypress', event => {
     const keyValue = event.key;
-    console.debug(keyValue)
-    performInputOperation(keyValue);
+    if (!isNewOperationAdding()) {
+        performInputOperation(keyValue);
+    }
 })
